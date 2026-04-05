a62ae97a990e5ca473135cbc540d6fdc607ae8f9
slick phantom <ynwghosted@icloud.com>
2026-04-05 08:16:16 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 /**
- * Knight Bot - A WhatsApp Bot
- * Copyright (c) 2024 Professor
+ * savt Bot - A WhatsApp Bot
+ * Copyright (c) 2024 slick
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the MIT License.
@@ -8,6 +8,7 @@
  * Credits:
  * - Baileys Library by @adiwajshing
  * - Pair Code implementation inspired by TechGod143 & DGXEON
+ * - knighbot
  */
 require('./settings')
 const { Boom } = require('@hapi/boom')
